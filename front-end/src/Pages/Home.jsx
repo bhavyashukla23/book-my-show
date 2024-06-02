6aeb455e0890e24c8dcbfbf0a1ea0c10c6529eec
unknown <bhavyashukla23@gmail.com>
2024-06-02 11:11:42 +0530

    added modal component and renamed file extenions

---
+++ front-end/src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Modal from '../Components/ModalComponent';
+
+const Home =() =>{
+
+    return(
+        <>
+         <Modal/>
+        </>
+    
+    );
+};
+
+export default Home;
