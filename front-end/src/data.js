90db44173db20b39c106a62164a5a97fc1f3d2f4
unknown <bhavyashukla23@gmail.com>
2024-06-03 11:41:44 +0530

    added data and related components

---
+++ front-end/src/data.js
@@ -0,0 +1,7 @@
+const movies = ["Queens Gambit" , "Top Gun : Maverick" , "MIB International" , "Pretty Woman"];
+const seats =["A1", "A2","B1","B2","C1","C2","D1","D2"];
+const slots= ["10:00 AM" , '01:00 PM', '03:00 PM', "08:00 PM"];
+
+exports.movies = movies;
+exports.seats = seats;
+exports.slots = slots;
